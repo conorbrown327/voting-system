d782365c2c92b5a238756147f782617c3997aa12
jacks <jacks@134.84.125.169>
2021-03-09 17:08:30 -0600

    Starting work on base classes and testing some code.

--- VotingSystem/src/election/ElectionDriver.java
+++ VotingSystem/src/election/ElectionDriver.java
@@ -4,7 +4,7 @@ public class ElectionDriver {
 	public static void main(String[] args) {
 		Party p1 = new Party("Republican");
 		Candidate c1 = new Candidate("Bob", p1);
-		Candidate c2 = new Candidate("Joe", p1);
+		Candidate c2 = new Candidate("Jill", p1);
 		Candidate c3 = new Candidate("Jack", p1);
 		
 		for (int i = 0; i < 3; i++) {
