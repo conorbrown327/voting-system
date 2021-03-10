a69fb82109e96b75eea32e93eab56862b3f27291
seancarter99 <72178005+seancarter99@users.noreply.github.com>
2021-03-09 19:34:44 -0600

    Added Ballot class functionality

--- VotingSystem/src/election/Ballot.java
+++ VotingSystem/src/election/Ballot.java
@@ -1,6 +1,23 @@
 package election;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class Ballot {
-	
+
+	private PriorityQueue<Candidate> votePreferences;
+
+	public Candidate findNextPreferredCandidate() {
+
+		return null;
+	}
+
+	public Queue<Candidate> getBallotQueue() {
+		return this.votePreferences;
+	}
+
+	public void addToQueue(Candidate c) {
+		this.votePreferences.add(c);
+	}
 
 }
