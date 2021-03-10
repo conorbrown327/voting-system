d11ca9a2ed1a258b62cec95c58b7b97ea595311f
seancarter99 <72178005+seancarter99@users.noreply.github.com>
2021-03-09 18:25:35 -0600

    Added Election class methods and stub for OPL/IR classes

--- VotingSystem/src/election/Election.java
+++ VotingSystem/src/election/Election.java
@@ -1,5 +1,34 @@
 package election;
 
+import java.io.FileWriter;
+import java.util.List;
+import java.util.Map;
+
 public abstract class Election {
 
+	private List<Candidate> candidates;
+	private FileWriter auditFile;
+	private int numCandidates;
+	private int numBallots;
+	private Map<String, Party> participatingParties;
+
+	protected Candidate breakTie(Candidate candidate1, Candidate candidate2) {
+		return null;
+	}
+
+	protected void determineWinner(String filePath) {
+		return;
+	}
+
+	protected Candidate getWinner() {
+		return null;
+	}
+
+	protected abstract void readBallotFile(String filePath);
+
+	protected abstract void writeToBallotFile(String line);
+
+	protected abstract void writeMediaFile();
+
+	protected abstract void writeResultsToTerminal();
 }
--- VotingSystem/src/election/InstantRunoffElection.java
+++ VotingSystem/src/election/InstantRunoffElection.java
@@ -2,4 +2,28 @@ package election;
 
 public class InstantRunoffElection extends Election {
 
+	@Override
+	protected void readBallotFile(String filePath) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void writeToBallotFile(String line) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void writeMediaFile() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void writeResultsToTerminal() {
+		// TODO Auto-generated method stub
+
+	}
+
 }
