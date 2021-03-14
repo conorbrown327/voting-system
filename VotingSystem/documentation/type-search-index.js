e2acfe6c822cfd45c8fe5ba258db8e9827a98b1b
Joe Cassidy <cassi083@umn.edu>
2021-03-14 13:52:48 -0500

    renaming VotingSystem

--- VotingSystem/documentation/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"election","l":"Ballot"},{"p":"election","l":"Candidate"},{"p":"election","l":"Election"},{"p":"election","l":"ElectionDriver"},{"p":"election","l":"InstantRunoffElection"},{"p":"election","l":"OpenPartyListingElection"},{"p":"election","l":"Party"}];updateSearchResults();
